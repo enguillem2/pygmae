e8047c67799ed62fbaf7a3f8df0b2f492347ce5a
enguillem <enguillem@gmail.com>
2023-10-30 13:59:11 +0100

    practicant amb rect

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ test_font = pygame.font.Font("font/Pixeltype.ttf",50)
 test_surface = pygame.Surface((100,200))
 test_surface.fill('Red')
 
-sky_surface= pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface= pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My Game',False,'Black')
 
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+vx=-2
+snail_x0=100
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80,sky_surface.get_height()))
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -24,6 +30,13 @@ while True:
   screen.blit(sky_surface,(0,0))
   screen.blit(ground_surface,(0,sky_surface.get_height()))
   screen.blit(text_surface,(300,50))
+  if snail_x0>0-snail_surface.get_width():
+    snail_x0=snail_x0+vx
+  else:
+    snail_x0=screen.get_width()+snail_surface.get_width()
+  screen.blit(snail_surface,(snail_x0,250))
+
+  screen.blit(player_surface,player_rect)
 
   pygame.display.update()
   clock.tick(60)
